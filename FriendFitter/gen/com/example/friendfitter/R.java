5a22adaaa6d57c5153f13362b6f807b994f97c58
oelhirai <oelhiraika@gmail.com>
2014-01-31 16:25:10 -0500

    Java File Upload
    instantiated the photo, menu, and login java files

--- FriendFitter/gen/com/example/friendfitter/R.java
+++ FriendFitter/gen/com/example/friendfitter/R.java
@@ -26,6 +26,9 @@ public final class R {
     }
     public static final class layout {
         public static final int login=0x7f030000;
+        public static final int menu=0x7f030001;
+        public static final int photo=0x7f030002;
+        public static final int screenstream=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
