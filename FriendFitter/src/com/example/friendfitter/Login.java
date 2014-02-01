ad6ad7e861cc18eaa01a95bf0ced6e302e2a6796
oelhirai <oelhiraika@gmail.com>
2014-01-31 20:07:06 -0500

    Moar UI editz

--- FriendFitter/src/com/example/friendfitter/Login.java
+++ FriendFitter/src/com/example/friendfitter/Login.java
@@ -2,44 +2,51 @@ package com.example.friendfitter;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-public class Login extends Activity implements View.OnClickListener{
-	
-	
+public class Login extends Activity implements View.OnClickListener {
+
 	Button login;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		instantiateButtons();
 		setContentView(R.layout.login);
+		instantiateVars();
 	}
 
-	private void instantiateButtons() {
+	private void instantiateVars() {
 		// TODO Auto-generated method stub
 		login = (Button) findViewById(R.id.loginbt);
 		login.setOnClickListener(this);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-
+	//this is where we may login, and later the password, name, and group should be registered here
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
-		case (R.id.loginbt) {
-			
-		}
-		}
-		
-	}
+		case (R.id.loginbt):
+			try {
+				Class mainMenu = Class.forName("com.example.friendfitter.Menu");
+				Intent menuIntent = new Intent(Login.this, mainMenu);
+				startActivity(menuIntent);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
 
+		}
+
+	}
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
 }
--- FriendFitter/src/com/example/friendfitter/Menu.java
+++ FriendFitter/src/com/example/friendfitter/Menu.java
@@ -1,1 +1,41 @@
 package com.example.friendfitter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Menu extends Activity implements View.OnClickListener {
+
+	Button takePicture;
+	Button streamPhotos;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		setContentView(R.layout.menu);
+		instantiateVars();
+		super.onCreate(savedInstanceState);
+	}
+
+	private void instantiateVars() {
+		// TODO Auto-generated method stub
+		takePicture = (Button) findViewById(R.id.takePhotobt);
+		streamPhotos = (Button) findViewById(R.id.photoStreambt);
+		takePicture.setOnClickListener(this);
+		streamPhotos.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case (R.id.takePhotobt):
+			break;
+
+		case (R.id.photoStreambt):
+			break;
+		}
+	}
+
+}
