96362febf597638c9b95792d01c2e0941f9c9085
oelhirai <oelhiraika@gmail.com>
2014-01-31 20:17:00 -0500

    Login Changes

--- FriendFitter/src/com/example/friendfitter/Login.java
+++ FriendFitter/src/com/example/friendfitter/Login.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends Activity implements View.OnClickListener {
 
-	Button login;
+	// YO GUYZ. These two are what they are,
+	Button login, register;
+	EditText group, password, name;
+	String groupS, passwordS, nameS;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +25,26 @@ public class Login extends Activity implements View.OnClickListener {
 	private void instantiateVars() {
 		// TODO Auto-generated method stub
 		login = (Button) findViewById(R.id.loginbt);
+		register = (Button) findViewById(R.id.registerbt);
+		group = (EditText) findViewById(R.id.groupNameet);
+		password = (EditText) findViewById(R.id.passwordet);
+		name = (EditText) findViewById(R.id.nameet);
 		login.setOnClickListener(this);
 	}
 
-	//this is where we may login, and later the password, name, and group should be registered here
+	// this is where we may login, and later the password, name, and group
+	// should be registered here
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
 		case (R.id.loginbt):
+			// The variables will contain the string corresponding to input fields
+			groupS = group.getText().toString();
+			passwordS = password.getText().toString();
+			nameS = name.getText().toString();
+			// So guys, this login right now just leads straight to the main menu
+			// we can change to to only go to the enu conditionally
 			try {
 				Class mainMenu = Class.forName("com.example.friendfitter.Menu");
 				Intent menuIntent = new Intent(Login.this, mainMenu);
@@ -39,10 +54,12 @@ public class Login extends Activity implements View.OnClickListener {
 				e.printStackTrace();
 			}
 			break;
-
+		case (R.id.registerbt):
+			break;
 		}
 
 	}
+
 	@Override
 	protected void onPause() {
 		// TODO Auto-generated method stub
