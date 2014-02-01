e6c6b24cfbcb70f68b4a08d5a215618bfa18c18a
oelhirai <oelhiraika@gmail.com>
2014-01-31 19:40:57 -0500

    Moar UIs

--- FriendFitter/gen/com/example/friendfitter/R.java
+++ FriendFitter/gen/com/example/friendfitter/R.java
@@ -16,13 +16,18 @@ public final class R {
         public static final int text_input_border=0x7f020002;
     }
     public static final class id {
-        public static final int editText1=0x7f070006;
-        public static final int editText2=0x7f070004;
+        public static final int button1=0x7f07000a;
+        public static final int button2=0x7f07000b;
         public static final int groupNameet=0x7f070002;
         public static final int groupNametv=0x7f070001;
-        public static final int menu_settings=0x7f070007;
-        public static final int nameet=0x7f070005;
+        public static final int loginbt=0x7f070007;
+        public static final int menu_settings=0x7f07000c;
+        public static final int nameet=0x7f070006;
+        public static final int nametv=0x7f070005;
+        public static final int ortv=0x7f070008;
+        public static final int passwordet=0x7f070004;
         public static final int passwordtv=0x7f070003;
+        public static final int registerbt=0x7f070009;
         public static final int welcometv=0x7f070000;
     }
     public static final class layout {
--- FriendFitter/src/com/example/friendfitter/Login.java
+++ FriendFitter/src/com/example/friendfitter/Login.java
@@ -3,13 +3,25 @@ package com.example.friendfitter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class Login extends Activity {
-
+public class Login extends Activity implements View.OnClickListener{
+	
+	
+	Button login;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		instantiateButtons();
 		setContentView(R.layout.login);
+	}
+
+	private void instantiateButtons() {
+		// TODO Auto-generated method stub
+		login = (Button) findViewById(R.id.loginbt);
+		login.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +29,17 @@ public class Login extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case (R.id.loginbt) {
+			
+		}
+		}
+		
 	}
 
 }
